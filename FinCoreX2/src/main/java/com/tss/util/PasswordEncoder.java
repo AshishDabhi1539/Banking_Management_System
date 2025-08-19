dd742e7824bfeaa1346c92b0b77356685d0cd805
Ashishkumar Dabhi <dabhiashish1539@gmail.com>
2025-08-19 22:48:49 +0530

    Fisrt Commit

---
+++ FinCoreX2/src/main/java/com/tss/util/PasswordEncoder.java
@@ -0,0 +1,13 @@
+package com.tss.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordEncoder {
+    public static String hash(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean matches(String rawPassword, String hashed) {
+        return BCrypt.checkpw(rawPassword, hashed);
+    }
+}
